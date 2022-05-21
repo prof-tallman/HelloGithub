93e82048dff938685e0590eb5b37cb9189627c8d
Prof Tallman <70240983+prof-tallman@users.noreply.github.com>
2022-05-21 16:40:27 -0700

    Update translator.py
    Changed from keyword to position based CLI

--- translator.py
+++ translator.py
@@ -10,33 +10,20 @@ import argparse
 def main():
     ''' Main program for Piglatin Translator CLI Program. '''
 
-    parser = argparse.ArgumentParser(
-        description='Translates between Pig Latin and English')
-    parser.add_argument(
-        '-p', '--phrase', type=str, help='message to translate', required=True)
-    parser.add_argument(
-        '-m', '--mode', type=str, choices=['piglatin', 'english'],
-        help='choose to translate to piglatin or english', required=True)
-    parser.add_argument(
-        '-v', '--verify', action='store_true',
-        help='check if the phrase contains invalid characters')
+    parser = argparse.ArgumentParser(description='Translates between Piglatin and English')
+    parser.add_argument('phrase', type=str, help='message to translate')
+    parser.add_argument('mode', type=str, choices=['piglatin', 'english'],
+                        help='choose to translate to piglatin or english')
     args = parser.parse_args()
 
     translator = piglatin.SimplifiedPiglatin()
-    if args.verify:
-        if args.piglatin and not translator.valid_phrase(args.piglatin):
-            print('Error: the phrase contains invalid characters.')
-            return
-        elif args.english and not translator.valid_phrase(args.english):
-            print('Error: the phrase contains invalid characters.')
-            return
-
-    if args.piglatin:
-        print(translator.encrypt(args.piglatin))
-    elif args.english:
-        print(translator.decrypt(args.english))
+    if args.mode == 'piglatin':
+        print(translator.encrypt(args.phrase))
+    elif args.mode == 'english':
+        print(translator.decrypt(args.phrase))
     else:
-        print('Error: no message to translate')
+        print('Error: unexpected translation mode')
+
     return
 
 
